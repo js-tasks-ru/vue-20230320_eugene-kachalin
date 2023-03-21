ac9c96be6b759f2fa615f6d21d8dd6697fcf7961
Eugene <ekachalin@gmail.com>
2023-03-21 21:06:20 +0300

    Solve VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,30 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const vm = createApp({
+    data() {
+        return {
+            a: 0,
+            b: 0,
+            operator: 'sum',
+        };
+    },
+    computed: {
+        result() {
+            switch (this.operator) {
+                case "sum":
+                    return this.a + this.b;
+                case "subtract":
+                    return this.a - this.b;
+                case "multiply":
+                    return this.a * this.b
+                case "divide": 
+                    if (this.b != 0) {
+                        return this.a / this.b
+                    }
+                    else return "Ошибка";;
+            }
+            return 777;
+        }
+    }
+
+}).mount('#app');

d573fb92c6311bbb556859167a42225008e2b899
Eugene <ekachalin@gmail.com>
2023-03-30 16:56:51 +0300

    Fixed CounterButton tests

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -3,7 +3,12 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'CounterButton',
 
-  props: ['count'],
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    }
+  },
   
   emits: ['update:count'],
 

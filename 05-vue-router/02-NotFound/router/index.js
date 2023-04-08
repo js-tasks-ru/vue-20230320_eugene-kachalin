d4f264be40757a8c44167faaf625a90d46f240cf
Eugene <ekachalin@gmail.com>
2023-04-08 15:55:13 +0300

    Solved 05-NotFound

--- 05-vue-router/02-NotFound/router/index.js
+++ 05-vue-router/02-NotFound/router/index.js
@@ -12,5 +12,9 @@ export const router = createRouter({
       path: '/page-b',
       component: () => import('../views/PageB'),
     },
+    {
+      path: '/:notFoundPath(.*)*',
+      component: () => import('../views/PageNotFound'),
+    },
   ],
 });

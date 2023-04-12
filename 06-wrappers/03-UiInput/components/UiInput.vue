338f308a6d210762316fd6d24c8aa6f9321db849
Eugene <ekachalin@gmail.com>
2023-04-12 16:18:52 +0300

    Solved 06-UiCheckbox

--- 06-wrappers/03-UiInput/components/UiInput.vue
+++ 06-wrappers/03-UiInput/components/UiInput.vue
@@ -1,13 +1,32 @@
 <template>
-  <div class="input-group input-group_icon input-group_icon-left input-group_icon-right">
-    <div class="input-group__icon">
-      <img class="icon" alt="icon" />
+  <div class="input-group" :class="{
+    'input-group_icon': $slots['left-icon'] || $slots['right-icon'],
+    'input-group_icon-left': $slots['left-icon'], 'input-group_icon-right': $slots['right-icon']
+  }">
+    <div v-if="$slots['left-icon']" class="input-group__icon">
+      <slot name="left-icon" />
     </div>
 
-    <input ref="input" class="form-control form-control_rounded form-control_sm" />
+    <!-- v-model="proxyModel" не работает с <component> -->
+    <!-- @input="$emit('update:modelValue', $event.target.value)" -->
 
-    <div class="input-group__icon">
-      <img class="icon" alt="icon" />
+    <component
+    :is="multiline ? 'textarea' : 'input'"
+    
+    ref="input" 
+    v-bind="$attrs" 
+
+    :value="modelValue" 
+    @input="$emit('update:modelValue', $event.target.value)"
+    
+    class="form-control"
+    :class="{
+      'form-control_rounded': rounded,
+      'form-control_sm': small
+    }" />
+    
+    <div v-if="$slots['right-icon']" class="input-group__icon">
+      <slot name="right-icon" />
     </div>
   </div>
 </template>
@@ -15,6 +34,47 @@
 <script>
 export default {
   name: 'UiInput',
+
+  inheritAttrs: false,
+
+  props: {
+    small: Boolean,
+    rounded: Boolean,
+    multiline: Boolean,
+    modelValue: String,
+    modelModifiers: {
+      // default: () => ({})
+    }
+  },
+
+  created() {
+    console.log(this.modelModifiers) // { capitalize: true }
+  },
+
+  emits: {
+    'update:modelValue': null,
+  },
+
+  computed: {
+    // не работает с <component>
+    // proxyModel: {
+    //   get() {
+    //     console.log(this.modelValue)
+    //     return this.modelValue;
+    //   },
+
+    //   set(value) {
+    //     console.log(value)
+    //     this.$emit('update:modelValue', value);
+    //   },
+    // },
+  },
+
+  methods: {
+    focus() {
+      this.$refs['input'].focus();
+    },
+  },
 };
 </script>
 

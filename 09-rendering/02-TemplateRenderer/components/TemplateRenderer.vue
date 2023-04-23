f5c1bf74f00282d487d83bf8f473ead21b1c3757
Eugene <ekachalin@gmail.com>
2023-04-23 19:20:30 +0300

    Not solved TemplateRenderer

--- 09-rendering/02-TemplateRenderer/components/TemplateRenderer.vue
+++ 09-rendering/02-TemplateRenderer/components/TemplateRenderer.vue
@@ -1,8 +1,11 @@
 <script>
-// import { compile } from 'vue';
-
+import { compile } from 'vue';
+// Works
+// import UiAlert from '/Users/.../vue-20230320_eugene-kachalin/09-rendering/02-TemplateRenderer/components/UiAlert.vue';
 export default {
   name: 'TemplateRenderer',
+
+  // components() {return this.components},
 
   props: {
     template: {
@@ -20,5 +23,21 @@ export default {
       default: () => [],
     },
   },
-};
+
+  // created() {
+  //   for (const name in this.components) {
+  //     // component(name)
+  //   }
+  // },
+
+  computed: {
+    renderFunction() {
+      return compile(this.template)
+    }
+  },
+
+  render(...args) {
+    return this.renderFunction.apply(this, args);
+  },
+}
 </script>

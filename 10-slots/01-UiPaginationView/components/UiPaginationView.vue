f50b948869233c41c86fed6248cf3806e6bdd7ae
Eugene <ekachalin@gmail.com>
2023-04-26 00:04:30 +0300

    Solve UiPaginationView

--- 10-slots/01-UiPaginationView/components/UiPaginationView.vue
+++ 10-slots/01-UiPaginationView/components/UiPaginationView.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="pagination-container">
-    <!-- Контент страницы -->
+    <slot :item="item" v-for="item in displayedItems" />
   </div>
 </template>
 
 <script>
 export default {
   name: 'UiPaginationView',
+
+  computed: {
+    displayedItems() {
+      const startIndex = (this.page - 1) * this.perPage;
+      const endIndex = startIndex + this.perPage;
+      return this.items.slice(startIndex, endIndex);
+    }
+  },
 
   props: {
     page: {

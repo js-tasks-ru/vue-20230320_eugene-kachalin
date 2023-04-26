eea95923bc560c6ba9a70651d2d038a8af195965
Eugene <ekachalin@gmail.com>
2023-04-26 14:49:40 +0300

    State based Calendar approach

---
+++ 10-slots/03-UiCalendarView/services/calendarService.js
@@ -0,0 +1,72 @@
+import dayjs from 'dayjs';
+import arraySupport from 'dayjs/plugin/arraySupport';
+import utc from 'dayjs/plugin/utc';
+import isToday from 'dayjs/plugin/isToday';
+import weekday from 'dayjs/plugin/weekday';
+import timezone from 'dayjs/plugin/timezone';
+import locale from 'dayjs/locale/ru';
+
+dayjs
+    .extend(weekday)
+    .extend(utc)
+    .extend(isToday)
+    .extend(arraySupport)
+    .extend(timezone)
+    .locale(locale);
+
+function getDays(date) {
+    const days = [];
+    const previousMonthDays = date
+        .date(1)
+        .weekday();
+
+    const currentMonthDays = date.daysInMonth();
+
+    // monday = 0 for dayjs.locale('ru')
+    const nextMonthDays = (7 - 1)
+        - date
+        .date(date.daysInMonth())
+            .weekday();
+
+    for (let day = 1; day <= previousMonthDays; day++) {
+        const shift = day - previousMonthDays;
+        days.push(createDay({ date, shift }));
+    }
+
+    for (let day = 1; day <= currentMonthDays; day++) {
+        const shift = day;
+        days.push(createDay({ date, shift }));
+    }
+
+    for (let day = 1; day <= nextMonthDays; day++) {
+        const shift = date.daysInMonth() + day;
+        days.push(createDay({ date, shift }));
+    }
+
+    return days;
+}
+
+function getTodayText(date) {
+    return date.toDate().toLocaleDateString(navigator.language, {
+        month: 'long',
+        year: 'numeric',
+    })
+}
+
+function createDay({date, shift}) {
+    const calendarDay = dayjs([
+        date.year(), date.month(), shift
+    ]);
+    
+    return {
+        date: calendarDay.date(),
+        isCurrentMonth: calendarDay.month() == date.month(),
+        // meetups: this._meetups[calendarDay.utc(calendarDay).valueOf()],
+        isToday: calendarDay.isToday(),
+    };
+}
+
+export {
+    getDays,
+    getTodayText,
+}
